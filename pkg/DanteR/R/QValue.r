9dd14add9a44395821a311dbb2aa07cb35c47e53
ttaverner (Tom Taverner) <t.taverner@gmail.com>
2010-12-10 17:42:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgtk2extras@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DanteR/R/QValue.r
+++
@@ -1,9 +0,0 @@
-  # Apply qvalue column-wise to matrix
-QValue <- function(p.value.arr){
-  p.value.df <- data.frame(na.omit(p.value.arr))
-  sapply(p.value.df, function(x) qvalue(x)$q)
-}
-
-QValue.dialog <- list(
-  p.value.arr.dataframeItem = "", label = "Array of P-values to Transform", tooltip = "Enter in the location of the p-values"
-)
